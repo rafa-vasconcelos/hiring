34d6ce89fc256f11a3eb54931e7d3fe6963f325b
Rafael Vasconcelos <rafaelvasconcelos@protonmail.com>
2025-03-18 13:30:22 -0300

    challenge done

--- backend/src/modules/students/students-controller.js
+++ backend/src/modules/students/students-controller.js
@@ -1,35 +1,53 @@
 const asyncHandler = require("express-async-handler");
-const { getAllStudents, addNewStudent, getStudentDetail, setStudentStatus, updateStudent } = require("./students-service");
+const {
+  getAllStudents,
+  getStudentDetail,
+  addNewStudent,
+  setStudentStatus,
+  updateStudent,
+} = require("./students-service");
 
 const handleGetAllStudents = asyncHandler(async (req, res) => {
-    //write your code
-
+  const { name, status } = req.query;
+  const students = await getAllStudents({ name, status });
+  res.json({ students });
 });
 
 const handleAddStudent = asyncHandler(async (req, res) => {
-    //write your code
-
+  const studentData = req.body;
+  const message = await addNewStudent(studentData);
+  res.status(201).json(message);
 });
 
 const handleUpdateStudent = asyncHandler(async (req, res) => {
-    //write your code
-
+  const { id: studentId } = req.params;
+  const updatedData = req.body;
+  const message = await updateStudent({ ...updatedData, studentId });
+  res.json(message);
 });
 
 const handleGetStudentDetail = asyncHandler(async (req, res) => {
-    //write your code
-
+  const { id } = req.params;
+  const student = await getStudentDetail(id);
+  res.json(student);
 });
 
 const handleStudentStatus = asyncHandler(async (req, res) => {
-    //write your code
-
+  const { id: studentId } = req.params;
+  const { status } = req.body;
+  const { id: reviewerId } = req.user;
+  const message = await setStudentStatus({
+    userId: studentId,
+    reviewerId,
+    status,
+  });
+  res.json(message);
 });
 
 module.exports = {
-    handleGetAllStudents,
-    handleGetStudentDetail,
-    handleAddStudent,
-    handleStudentStatus,
-    handleUpdateStudent,
+  handleGetAllStudents,
+  handleGetStudentDetail,
+  handleAddStudent,
+  handleStudentStatus,
+  handleUpdateStudent,
 };
